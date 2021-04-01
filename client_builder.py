79fafa22f32a3f1be4ab9cc7847dce3e70506a2d
Max Wood <max.w29@hotmail.com>
2021-04-01 15:45:56 -0500

    added client builder

---
+++ client_builder.py
@@ -0,0 +1,26 @@
+# client builder 
+import atexit
+
+import tda
+
+from settings import CALLBACK_URL, CONSUMER_KEY, ACCOUNT_ID, CHROMEDRIVER_DIR, CHROME_DIR
+
+client_id = CONSUMER_KEY + "@AMER.OAUTHAP"
+path_to_token = "./tokens/token.json"
+
+def make_driver():
+    from selenium import webdriver
+    from selenium.webdriver.chrome.options import Options
+    
+    options = Options()
+    options.binary_location = CHROME_DIR
+    driver = webdriver.Chrome(CHROMEDRIVER_DIR, chrome_options=options)
+    atexit.register(lambda: driver.quit())
+    return driver
+
+def build_client():
+    client = tda.auth.easy_client(api_key=client_id, redirect_uri=CALLBACK_URL, token_path=path_to_token, webdriver_func=make_driver, asyncio=False)
+    return client
+
+if __name__ == "__main__":
+    build_client()
