7812eae19f27e661b745a2e76eeb7fa4405d2fe6
Max Wood <max.w29@hotmail.com>
2021-04-25 00:19:29 -0500

    fixed webdriver updater

--- webdriver_updater.py
+++ webdriver_updater.py
@@ -1,31 +1,96 @@
-import sys
+import glob
+import os
 import re
+from shutil import move, copyfile
+import sys
+import time
+import zipfile
 
-import requests
 from bs4 import BeautifulSoup
+import requests
 from selenium import webdriver
+from splinter import Browser
 
+# these will need to be setup manually the first go around
+from settings import CHROMEDRIVER_DIR1, CHROMEDRIVER_DIR2
 
 def get_version_links(href):
     return href and re.compile("chromedriver.storage.googleapis.com").search(href)
 
 def get_html(url):
-    # url = 'https://chromedriver.chromium.org/downloads'
     r = requests.get(url)
     html = r.text
     return html
 
-soup = BeautifulSoup(get_html('https://chromedriver.chromium.org/downloads'), 'lxml')
+def get_updates():
+    soup = BeautifulSoup(get_html('https://chromedriver.chromium.org/downloads'), 'lxml')
 
-version_links = soup.find_all(href=get_version_links)
+    version_links = soup.find_all(href=get_version_links)
 
-# print(version_links[0]['href'])
-# print(version_links[1]['href'])
-# print(version_links[2]['href'])
+    # print(version_links[0]['href'])
+    # print(version_links[1]['href'])
+    # print(version_links[2]['href'])
 
-dl_link0 = get_html(version_links[0]['href'])
-dl_link1 = get_html(version_links[1]['href'])
-dl_link2 = get_html(version_links[2]['href'])
+    dl_link0 = version_links[0]['href']
+    dl_link1 = version_links[1]['href']
+    dl_link2 = version_links[2]['href']
 
-soup = BeautifulSoup(dl_link0, 'lxml')
-print(soup)
+    dl_links = [dl_link0, dl_link1, dl_link2]
+
+    # download new versions
+    count = 1
+    for link in dl_links:
+        executable_path = {'executable_path': CHROMEDRIVER_DIR1}
+        options = webdriver.ChromeOptions()
+        prefs = {
+            "download.default_directory" : "./chromedrivers",
+            "download.directory_upgrade": "true",
+            "download.prompt_for_download": "false",
+            "disable-popup-blocking": "true"
+        }
+        options.add_argument("start-maximized")
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        options.add_experimental_option('useAutomationExtension', False)
+        options.add_experimental_option("prefs", prefs)
+        try:
+            browser = Browser('chrome', **executable_path, headless=False, incognito=True, options=options)
+            browser.visit(link)
+        except:
+            executable_path = {'executable_path': CHROMEDRIVER_DIR2}
+            browser = Browser('chrome', **executable_path, headless=False, incognito=True, options=options)
+            browser.visit(link)
+
+        time.sleep(1)
+        dl_button = browser.links.find_by_partial_href('chromedriver_win32.zip')
+        time.sleep(1)
+        dl_button.click()
+        time.sleep(1)
+
+        browser.quit()
+        time.sleep(1)
+
+        download_path = 'C:\\Users\\maxw2\\Downloads\\'
+        new_path = os.getcwd()
+        old_file_name = download_path+'chromedriver_win32.zip'
+        new_file_name = new_path+f'/chromedrivers/chromedriver_win32-{count}.zip'
+        count += 1
+        move(old_file_name, new_file_name)
+        time.sleep(1)
+
+    # remove old versions
+    list_of_files = [glob.glob("./chromedrivers/1/*.exe")]
+    list_of_files.append(glob.glob("./chromedrivers/2/*.exe")) 
+    list_of_files.append(glob.glob("./chromedrivers/3/*.exe")) 
+    for file_ in list_of_files:
+        os.remove(file_[0])
+
+    # unzip and remove zips
+    list_of_files = glob.glob("./chromedrivers/*.zip") 
+    sorted_files = sorted(list_of_files, key=os.path.getctime)
+    # print(sorted_files)
+    count = 1
+    for file_ in sorted_files:
+        with zipfile.ZipFile(file_, 'r') as zip_ref:
+            zip_ref.extractall(f"./chromedrivers/{count}/")
+            count += 1
+        os.remove(file_)
