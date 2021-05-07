0ca8533fb31a6e34d21e5f038f37c8aa57675be8
Max Wood <max.w29@hotmail.com>
2021-05-07 08:04:50 -0500

    clean up files deletes old csv files now

--- file_cleanup.py
+++ file_cleanup.py
@@ -2,6 +2,7 @@ import datetime, time, csv, threading, sys, subprocess
 import glob
 from pathlib import Path
 import os
+import shutil
 
 folder_names = ["csv's", "csv's/monitors", "csv's/reports", "csv's/trades", "csv's/watches"]
 
@@ -16,6 +17,25 @@ def delete_logs():
     if len(sorted_logs) > 2:
         os.remove(sorted_logs[0])
         delete_logs()
+
+def delete_old_folders():
+    raw = glob.glob("./csv's/raw/*/")
+    sorted_raw = sorted(raw, key=os.path.getctime)
+    monitors = glob.glob("./csv's/monitors/*/")
+    sorted_monitors = sorted(monitors, key=os.path.getctime)
+    reports = glob.glob("./csv's/reports/*/")
+    sorted_reports = sorted(reports, key=os.path.getctime)
+    trades = glob.glob("./csv's/trades/*/")
+    sorted_trades = sorted(trades, key=os.path.getctime)
+    watches = glob.glob("./csv's/watches/*/")
+    sorted_watches = sorted(watches, key=os.path.getctime)
+    folders = [sorted_raw, sorted_monitors, sorted_reports, sorted_trades, sorted_watches]
+    # print(folders)
+    for folder in folders:
+        print(len(folder))
+        while len(folder) > 15:
+            shutil.rmtree(folder[0], ignore_errors=True)
+            folder.pop(0)
 
 def cleanup_files():
     
@@ -34,6 +54,8 @@ def cleanup_files():
                 os.rename(file_, f'./{folder}/{current_date}{just_file_name}')
 
     delete_logs()
+    delete_old_folders()
 
 if __name__ == "__main__":
     cleanup_files()
+    # delete_old_folders()
